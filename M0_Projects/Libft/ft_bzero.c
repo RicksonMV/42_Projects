33f5ed07d07a845208bb621414aa1c0d851706d9
Rickson Babosa <rickson.vide@gmail.com>
2025-10-24 20:13:57 +0100

    começar malloc

--- M0_Projects/Libft/ft_bzero.c
+++ M0_Projects/Libft/ft_bzero.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_bzero(void *str, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
 
+	ptr = (unsigned char *)str;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}
--- M0_Projects/Libft/func_ready/ft_memset.c
+++ M0_Projects/Libft/func_ready/ft_memset.c
@@ -12,14 +12,14 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*ptrto;
 
 	i = 0;
 	ptrto = (unsigned char *)s;
 	while (i < n)
 	{
-		ptrto[i] (unsigned char)= c;
+		ptrto[i](unsigned char) = c;
 		i++;
 	}
 	return (s);
