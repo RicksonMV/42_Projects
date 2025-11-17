a2ce34d9471206172c34801b5e04eecda6cc8815
Rickson Babosa <rickson.vide@gmail.com>
2025-11-17 11:03:43 +0000

    calloc feito

--- M0_Projects/Libft/func_ready/ft_calloc.c
+++ M0_Projects/Libft/func_ready/ft_calloc.c
@@ -10,10 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_calloc(size_t count, size_t size)
+#include "libfh.h"
+
+void	ft_bzero(void *s, size_t n)
 {
+	char	*ptr;
 	size_t	i;
 
-	i = *count;
-
+	i = 0;
+	char ptr = (char *)s;
+	while(i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
 }
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
+
+	if (size != 0 && (nmemb > (SIZE_MAX / size)))
+		return (NULL);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, total_size);
+	return (ptr);
